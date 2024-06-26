38bafd7e632cfd25c2756749aa7efdc728177b78
Dom <domeccleston@gmail.com>
2024-06-26 16:12:19 +0100

    og image

--- ai-chatbot/app/layout.tsx
+++ ai-chatbot/app/layout.tsx
@@ -9,43 +9,58 @@ import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { cn } from "@/lib/utils";
 
 export const metadata = {
-  metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
-  title: {
-    default: "Next.js AI Chatbot",
-    template: "%s - Next.js AI Chatbot",
-  },
-  description: "An AI-powered chatbot template built with Next.js and Vercel.",
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
+	metadataBase: new URL(`https://${process.env.VERCEL_URL}`),
+	title: {
+		default: "Unkey semantic caching chatbot",
+	},
+	description: "AI chatbot with built-in semantic caching",
+	openGraph: {
+		title: "Unkey semantic caching chatbot",
+		description: "AI chatbot with built-in semantic caching",
+		images: [
+			{
+				url: "https://unkey.com/images/blog-images/covers/semantic-cache.png",
+				width: 1200,
+				height: 675,
+			},
+		],
+	},
+	twitter: {
+		title: "Unkey semantic caching chatbot",
+		card: "summary_large_image",
+	},
 };
 
 export const viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "white" },
+		{ media: "(prefers-color-scheme: dark)", color: "black" },
+	],
 };
 
 interface RootLayoutProps {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={cn("font-sans antialiased", GeistSans.variable, GeistMono.variable)}>
-        <Toaster />
-        <Providers attribute="class" defaultTheme="system" enableSystem>
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex flex-col flex-1 bg-muted/50">{children}</main>
-          </div>
-          <TailwindIndicator />
-        </Providers>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={cn(
+					"font-sans antialiased",
+					GeistSans.variable,
+					GeistMono.variable,
+				)}
+			>
+				<Toaster />
+				<Providers attribute="class" defaultTheme="system" enableSystem>
+					<div className="flex flex-col min-h-screen">
+						<Header />
+						<main className="flex flex-col flex-1 bg-muted/50">{children}</main>
+					</div>
+					<TailwindIndicator />
+				</Providers>
+			</body>
+		</html>
+	);
 }
