e8112536e792dcfa730408922c4c9909f5516760
chronark <dev@chronark.com>
2025-07-31 15:30:09 +0200

    chore: update hono to v2

--- hono/src/index.ts
+++ hono/src/index.ts
@@ -1,12 +1,21 @@
 import { Hono } from "hono";
-import { unkey, UnkeyContext } from "@unkey/hono";
+import { unkey, type UnkeyContext } from "@unkey/hono";
 
 const app = new Hono<{ Variables: { unkey: UnkeyContext } }>();
 
-app.use("*", unkey());
+app.use(
+  "*",
+  unkey({
+    rootKey: "unkey_3ZGW4LqobX2MLpVBs9uxHxsB",
+  }),
+);
 
 app.get("/", (c) => {
-	return c.text("Hello Hono!");
+  const unkey = c.get("unkey");
+  if (!unkey.data.valid) {
+    return c.text("Go away");
+  }
+  return c.text("Hello Hono!");
 });
 
 export default app;
